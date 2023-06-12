6d6eb02bec0c2bbd6b26fa55140f3ab6e83e559e
sjgarc20 <127725103+sjgarc20@users.noreply.github.com>
2023-06-12 15:31:58 -0600

    Blackbox tests forgot to push earlier, doing this now

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -79,4 +79,189 @@ public class BlackBoxGiven {
         double amount = cart1.calcCost();
         assertEquals(cart1Expected, amount, .01);
     }
+    
+    
+
+    
+    @Test
+    public void costOfDairyCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Dairy());
+        
+        assertEquals(3.24, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfMeatCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Meat());
+        
+        assertEquals(10.8, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfProduceCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Produce());
+        
+        assertEquals(2.16, test_cart.calcCost(), .01);
+        
+        test_cart.addItem(new Produce());
+        
+        assertEquals(4.32, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfAlcoholCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Alcohol());
+        
+        assertEquals(8.64, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfFrozenFoodCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new FrozenFood());
+        
+        assertEquals(5.4, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfAlcholAndFrozenFoodCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Alcohol());
+        test_cart.addItem(new FrozenFood());
+        
+        assertEquals(10.8, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfCorrect() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        test_cart.addItem(new Dairy());
+        
+        assertEquals(3.24, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void costOfProduce3for5Correct() throws Exception {
+        Cart test_cart = createCart(25);
+        
+        for(int i = 0; i < 3; i++) {
+        	test_cart.addItem(new Produce());
+        }
+        
+        assertEquals(5.4, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void multiple3for5ProduceDeal() throws Exception {
+    	Cart test_cart = createCart(25);
+        
+        for(int i = 0; i < 6; i++) {
+        	test_cart.addItem(new Produce());
+        }
+        
+        assertEquals(10.8, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void multipleTypesOfDeals() throws Exception {
+    	Cart test_cart = createCart(25);
+        
+    	test_cart.addItem(new Alcohol());
+    	test_cart.addItem(new FrozenFood());
+    	test_cart.addItem(new Alcohol());
+    	test_cart.addItem(new FrozenFood());
+        for(int i = 0; i < 6; i++) {
+        	test_cart.addItem(new Produce());
+        }
+        
+        assertEquals(32.4, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void alcoholAgeLimitEnforced() throws Exception, UnderAgeException {
+        Cart test_cart = createCart(0);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Under Aged shopper was allowed alcohol!", 8.64, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(1);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Under Aged shopper was allowed alcohol!", 8.64, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(19);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Under Aged shopper was allowed alcohol!", 8.64, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(20);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Under Aged shopper was allowed alcohol!", 8.64, test_cart.calcCost(), .01);
+    }
+    
+    @Test
+    public void legalAgedShoppersAllowedAlcohol() throws Exception, UnderAgeException {
+    	Cart test_cart = createCart(21);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Legal Age shopper was denied alcohol!", 0, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(22);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Legal Age shopper was denied alcohol!", 0, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(30);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Legal Age shopper was denied alcohol!", 0, test_cart.calcCost(), .01);
+        
+        test_cart = createCart(100);
+        test_cart.addItem(new Alcohol());
+        assertNotEquals("Legal Age shopper was denied alcohol!", 0, test_cart.calcCost(), .01);
+    }
+//    @Test
+//    public void noFalseUnderAgeCatches() throws Exception {
+//    	Cart test_cart = createCart(0);
+//        try { 
+//        	test_cart.addItem(new Dairy());
+//        } catch (Exception e) {
+//        	assertEquals("If Failed then False Alcohol Flag!", 3.24, test_cart.calcCost(), .01);
+//        }
+//        
+//        test_cart = createCart(1);
+//        try {
+//        	test_cart.addItem(new Dairy());
+//        } catch (Exception e) {
+//        	assertEquals("If Failed then False Alcohol Flag!", 3.24, test_cart.calcCost(), .01);
+//        
+//        test_cart = createCart(19);
+//        	test_cart.addItem(new Dairy());
+//        	assertEquals("If Failed then False Alcohol Flag!", 3.24, test_cart.calcCost(), .01);
+//        
+//        test_cart = createCart(20);
+//        	test_cart.addItem(new Dairy());
+//        	assertEquals("If Failed then False Alcohol Flag!", 3.24, test_cart.calcCost(), .01);
+//        	
+//        test_cart = createCart(21);
+//        	test_cart.addItem(new Dairy());
+//        	assertEquals("If Failed then False Under Age Catch!", 0, test_cart.calcCost(), .01);
+//        
+//        test_cart = createCart(22);
+//        test_cart.addItem(new Dairy());
+//        	assertEquals("If Failed then False Under Age Catch!", 0, test_cart.calcCost(), .01);
+//        
+//        test_cart = createCart(30);
+//       	test_cart.addItem(new Dairy());
+//        assertEquals("If Failed then False Under Age Catch!", 0, test_cart.calcCost(), .01);
+//        
+//        test_cart = createCart(100);
+//        test_cart.addItem(new Dairy());
+//        assertEquals("If Failed then False Under Age Catch!", 0, test_cart.calcCost(), .01);
+//    }
 }
